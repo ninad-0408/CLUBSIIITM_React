d643ae6859de9af3c25b7aaa8ca19e2610adec29
Akshat Gupta <akshatg450@gmail.com>
2021-11-11 19:00:33 +0530

    added various functionalities related to clubs in api and actions

--- src/Constants/actionTypes.js
+++ src/Constants/actionTypes.js
@@ -1,1 +1,6 @@
 export const CLUBS = 'CLUBS';
+export const CLUB = 'CLUB';
+export const PATCH = "PATCH";
+export const EVENT = "EVENT";
+export const APPROVAL = "APPROVAL";
+export const REMOVE = "REMOVE";
