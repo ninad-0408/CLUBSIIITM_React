9db0a40b346bf51dfefc401292d0b95e215f6bc8
Ninad Kalanke <ninadkalanke@gmail.com>
2021-11-12 21:51:21 +0530

    actions fixed

--- src/Api/index.js
+++ src/Api/index.js
@@ -5,14 +5,9 @@ const url = 'https://clubs-iiitm-server.herokuapp.com';
 const API = axios.create({ baseURL: url });
 
 export const getClubs = () => API.get('/club');
-
-export const getClub = () => await API.get('/club/:clubId');
-
-export const patchClub = (updateInfo) => API.patch(`/club/:clubId`, updateInfo);
-
-export const postEvent  = (newEvent) => API.post(`/club/:clubId/event`, newEvent);
-
-export const postApproval = (Approval) => API.post("/club/:clubId/approval", Approval);
-
-export const removeMember = (student) => API.post("/club/:clubId/remove/:studentId", student);
+export const getClub = (clubId) => API.get(`/club/${clubId}`);
+export const patchClub = (clubId, updateInfo) => API.patch(`/club/${clubId}`, updateInfo);
+export const postEvent  = (clubId, newEvent) => API.post(`/club/${clubId}/event`, newEvent);
+export const postApproval = (clubId, approval) => API.post(`/club/${clubId}/approval`, approval);
+export const removeMember = (clubId, studentId) => API.post(`/club/${clubId}/remove/${studentId}`);
 
