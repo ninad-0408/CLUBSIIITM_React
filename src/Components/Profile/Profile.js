08d8465585e45b96e103b6688d0ca51fe97d5a37
Akshat Gupta <akshatg450@gmail.com>
2021-11-27 17:27:17 +0530

    font changes in profile

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -24,51 +24,51 @@ const Profile = () => {
             <>
                 <div className="profile" id="profile" style={{ padding: '11rem 0' }}>
 
-                    <div class="container-fluid text-center mt-3" id='backgroundimage   '>
-                        <h1>
+                    <div class="container-fluid text-center mt-3" id='backgroundimage   '  >
+                        <h1 style={{ 'font-family': 'Permanent Marker,cursive' }}>
                             {student.name}
                         </h1>
                     </div>
                     <div class="col mt-3 text-center">
-                        <div class="container h5">
-                            <div class="container font-weight-bold d-4">
+                        <div class="container h5" style={{ 'font-family': 'Permanent Marker, cursive' }}>
+                            <div class="container font-weight-bold d-4" >
                                 {student.bio}
                             </div>
                             <div class="text-center font-weight-light">- {student.name}
                             </div>
                         </div>
 
-                        <div class="mt-4 h4">
-                            <span class="font-weight-bold">Email Address:</span>
+                        <div class="mt-4 h4" style={{ 'font-family': 'Permanent Marker, cursive' }}>
+                            <span class="font-weight-bold">Email Address: </span>
                             <a href={`mailto:${student.email}`}>
                                 {student.email}
                             </a>
                         </div>
 
-                        <div class="mt-4 h4">
-                            <span class="font-weight-bold">LinkedIn:</span>
+                        <div class="mt-4 h4" style={{ 'font-family': 'Permanent Marker, cursive' }}>
+                            <span class="font-weight-bold">LinkedIn: </span>
                             <a href={`https://www.linkedin.com/in/${student.linkedin}`}>
                                 {student.linkedin}
                             </a>
                         </div>
 
-                        <div class="mt-4 h4">
-                            <span class="font-weight-bold mr-2">Branch:</span>
+                        <div class="mt-4 h4" style={{ 'font-family': 'Permanent Marker, cursive' }}>
+                            <span class="font-weight-bold mr-2">Branch: </span>
                             {student.branch}
                         </div>
 
-                        <div class="mt-4 h4">
-                            <span class="font-weight-bold mr-2">Roll No.:</span>
+                        <div class="mt-4 h4" style={{ 'font-family': 'Permanent Marker, cursive' }}>
+                            <span class="font-weight-bold mr-2">Roll No.: </span>
                             {student.rollNo}
                         </div>
 
-                        <div class="mt-4 h4">
-                            <span class="font-weight-bold mr-2">Contact No.:</span>
+                        <div class="mt-4 h4" style={{ 'font-family': 'Permanent Marker, cursive' }}>
+                            <span class="font-weight-bold mr-2">Contact No.: </span>
                             {student.phoneno}
                         </div>
 
-                        <div class="mt-4 h4">
-                            <span class="font-weight-bold mr-2">Batch:</span>
+                        <div class="mt-4 h4" style={{ 'font-family': 'Permanent Marker, cursive' }}>
+                            <span class="font-weight-bold mr-2">Batch: </span>
                             {student.batch}
                         </div>
                     </div>
