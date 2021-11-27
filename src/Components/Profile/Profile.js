8442077665906a219a98c1d2b569730446af6516
Akshat Gupta <akshatg450@gmail.com>
2021-11-27 17:50:31 +0530

    added styles to profile page

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -22,7 +22,7 @@ const Profile = () => {
 
         (student !== undefined) ?
             <>
-                <div className="profile" id="profile" style={{ padding: '11rem 0' }}>
+                <div className="profile" id="profile" style={{ padding: '6rem 0' }}>
 
                     <div class="container-fluid text-center mt-3" id='backgroundimage   '  >
                         <h1 style={{ 'font-family': 'Permanent Marker,cursive' }}>
@@ -40,14 +40,14 @@ const Profile = () => {
 
                         <div class="mt-4 h4" style={{ 'font-family': 'Permanent Marker, cursive' }}>
                             <span class="font-weight-bold">Email Address: </span>
-                            <a href={`mailto:${student.email}`}>
+                            <a href={`mailto:${student.email}`} style={{'color':'#007bff'}}>
                                 {student.email}
                             </a>
                         </div>
 
                         <div class="mt-4 h4" style={{ 'font-family': 'Permanent Marker, cursive' }}>
                             <span class="font-weight-bold">LinkedIn: </span>
-                            <a href={`https://www.linkedin.com/in/${student.linkedin}`}>
+                            <a href={`https://www.linkedin.com/in/${student.linkedin}`} style={{'color':'#007bff'}}>
                                 {student.linkedin}
                             </a>
                         </div>
@@ -67,10 +67,19 @@ const Profile = () => {
                             {student.phoneno}
                         </div>
 
-                        <div class="mt-4 h4" style={{ 'font-family': 'Permanent Marker, cursive' }}>
+                        <div class="mt-4 h4" style={{ 'font-family': 'Permanent Marker, cursive' ,'marginBottom':'0'}}>
                             <span class="font-weight-bold mr-2">Batch: </span>
                             {student.batch}
                         </div>
+                        <a href="edit"  class="mt-5 btn btn-primary btn-lg" style={{'margin-right':'10px','background-color:':'blue'}}>
+						<i class="fas fa-edit"></i>
+						Edit Profile
+					</a>
+
+					<a href="/logout" class="mt-5 btn btn-danger btn-lg">
+						<i class="fas fa-sign-out-alt"></i>
+						Logout
+					</a>
                     </div>
                 </div>
             </> : <Loader margin />
